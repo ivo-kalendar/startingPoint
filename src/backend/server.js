87d572ca908ca1cd40355bf88f01f888ecbf9bce
Ivo <ivokalendar@icloud.com>
2020-02-01 11:22:17 +0100

    Test Production...

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,12 +2,12 @@
 const express = require('express')
 const app = express()
 
-const webpack = require('webpack')
-const config = require('../config/webpack.config.js')
-const compiler = webpack(config)
+// const webpack = require('webpack')
+// const config = require('../config/webpack.config.js')
+// const compiler = webpack(config)
 
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')(compiler)
+// const webpackDevMiddleware = require('webpack-dev-middleware')
+// const webpackHotMiddleware = require('webpack-hot-middleware')(compiler)
 
 
 app.set('views', './src/views')
@@ -18,10 +18,10 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 //  comment the next line if you run in production  //
-app.use(webpackDevMiddleware(compiler, config.devServer))
+// app.use(webpackDevMiddleware(compiler, config.devServer))
 
 
-app.use(webpackHotMiddleware)
+// app.use(webpackHotMiddleware)
 app.use(express.static('./dist'))
 
 
