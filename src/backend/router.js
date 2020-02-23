a71c44412b336dc31f3763ccce0cb226967809ea
Ivo <ivokalendar@icloud.com>
2020-02-23 17:02:13 +0100

    Ready for a first pull...

--- src/backend/router.js
+++ src/backend/router.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
-const views = path.join(__dirname, '../views/')
 
-const user1 = 'Hello from backend/router.js'
-const consoleLog = function() { console.log('I am writing in the server console...')}
-
-router.get('/', (req,res) => res.render('index', {user: user1, theConsole: consoleLog()}))
-router.get('/about', (req,res) => res.render('about', {user: user1, theConsole: consoleLog()}))
-router.get('/test', (req,res) => res.sendFile(path.join(`${views}test.html`)))
-router.get('/another', (req,res) => res.render('another', {views: views,user: user1, theConsole: consoleLog()}))
+router.get('/', (req,res) => res.render('index'))
 
 module.exports = router
--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,44 +1,4 @@
-
-// import 'webpack-hot-middleware/client?reload=true'
-
-//--VIEWS--// use for hot-reload only for development env //
 require('../views/test.html')
 require('../views/index.ejs')
-require('../views/about.ejs')
-require('../views/another.ejs')
-require('../views/includeFile.ejs')
 
-//--STYLES--//
 require('../styles/main.css')
-
-console.log(`You are in ${process.env.NODE_ENV} environment.`)
-
-
-// //--SCRIPTS--//
-// require('./debug/debug')
-// const {object} = require('./debug/object')
-import obj from './debug/object'
-// import sec from './debug/debug'
-import consoleLogs from './debug/debug'
-// const { consoleLogs, sec } = require('./debug/debug')
-// const { obj } = require('./debug/object')
-
-//--TEST-CODE--//
-consoleLogs.func1()
-consoleLogs.func3()
-consoleLogs.func5()
-// sec()
-obj.func1()
-// obj.func2()
-obj.func3()
-// obj.func4()
-obj.func5()
-
-// let arrFunct = async args => {
-// 	const { aqwqw, bqwqw} = args
-// 	await console.log(`Hello from the future... ${aqwqw} , ${bqwqw}`)
-// 	console.log('Done!!!')
-// }
-
-
-// arrFunct({ aqwqw: 5, bqwqw: 2 })
